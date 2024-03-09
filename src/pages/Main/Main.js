5c3d16d2afa548d6af480301a96e7df15392e23a
v-vinay <v.vinayvarma1@gmail.com>
2024-03-08 21:11:36 -0500

    Fixed bugs and updated resume

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-import { Navbar, Footer, Landing, About, Skills, Testimonials, Blog, Education, Experience, Contacts, Projects, Services, Achievement } from '../../components'
+import { Navbar, Footer, Landing, About, Skills, Testimonials, Education, Experience, Contacts, Projects, Achievement } from '../../components'
 import { headerData } from '../../data/headerData'
 
 function Main() {
@@ -19,9 +19,7 @@ function Main() {
             <Experience />
             <Projects />
             <Achievement />
-            <Services />
             <Testimonials />
-            <Blog />
             <Contacts />
             <Footer />
         </div>
